89ec3ca73c1d2315a525d334e8d10b318b59c21a
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-06-04 15:05:36 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE series (
   title TEXT,
   author_id INTEGER,
   subgenre_id INTEGER);
-  
+
 CREATE TABLE subgenres (id INTEGER PRIMARY KEY, name TEXT);
 CREATE TABLE authors (id INTEGER PRIMARY KEY, name TEXT);
 CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id INTEGER);
