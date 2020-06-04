1a120b267f9b5c1fe689c8c229d2d1928f75cfc9
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-06-04 17:09:39 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -3,7 +3,6 @@ def select_books_titles_and_years_in_first_series_order_by_year
   SELECT books.title, books.year
   FROM books INNER JOIN series ON books.series_id = series.id
   ORDER BY books.year
-  LIMIT 1
   "
 end
 
