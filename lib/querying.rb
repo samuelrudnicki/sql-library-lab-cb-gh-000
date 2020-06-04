115daf9c7ad9465ead3b1fa41f863f6bac297445
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-06-04 20:17:37 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -47,5 +47,8 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "
+  SELECT characters.name, count(book_id)
+  FROM characters INNER JOIN character_books
+  "
 end
