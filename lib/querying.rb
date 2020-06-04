55dd0840dad75d94463fb9d4c11d05f5c3e369b3
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-06-04 20:05:08 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,7 +35,11 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "
+  SELECT series.title
+  FROM series INNER JOIN authors ON series.author_id = authors.id
+              INNER JOIN characters ON authors.id = characters.author_id
+  "
 end
 
 def select_character_names_and_number_of_books_they_are_in
